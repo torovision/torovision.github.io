6ab5455a2072db2fde58750bec6a8f8329b4fc3e
torovision <torovision@users.noreply.github.com>
2026-04-09 23:44:52 +0100

    Configure GitHub Pages and private visibility

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ouni-pos-frontend/',
   plugins: [react()],
   server: {
     allowedHosts: true,
