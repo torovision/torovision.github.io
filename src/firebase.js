81e69e09e5b7faee1f01be1f54d37668200fc5da
torovision <torovision@users.noreply.github.com>
2026-04-10 00:27:15 +0100

    Config: Inject active Firebase database keys to enable global syncing

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 
 // ⚠️ PASTE YOUR COPIED FIREBASE CONFIG OVER THIS OBJECT:
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyC7YDEeDlUK5wEnSV90k9-9jeVhpufXTRQ",
+  authDomain: "ouni-8d935.firebaseapp.com",
+  projectId: "ouni-8d935",
+  storageBucket: "ouni-8d935.firebasestorage.app",
+  messagingSenderId: "529988333963",
+  appId: "1:529988333963:web:2c99a842bca83a9e1a3f8e",
+  measurementId: "G-78TXZ38ZN0"
 };
 
 // We check if you have pasted the keys yet
